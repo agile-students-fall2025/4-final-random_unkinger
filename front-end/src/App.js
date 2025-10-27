47e9778bf84bf007af01d316df94eb3629ceb278
Pranathi Chinthalapani <prc9852@nyu.edu>
2025-10-27 15:51:24 -0400

    Implement ActivityTracking frontend components and update App.js/App.css

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 import Profile from "./Components/Profile/Profile";
 import Search from "./Components/Search/Search";
+import ActivityTracking from "./Components/ActivityTracking/ActivityTracking";
 
 function App() {
   return (
     <div>
+      {/* Uncomment the page you want to see: */}
       <LoginSignup/>
+      {/* <Profile /> */}
+      {/* <Search /> */}
+      {/* <ActivityTracking /> */}
+
     </div>
   );
 }
