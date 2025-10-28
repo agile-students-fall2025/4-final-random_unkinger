1b59c660b6ac838292308f653743ae90f4ac42ac
kazisean <keh8423@nyu.edu>
2025-10-28 10:09:33 -0400

    added scan food option

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,15 +3,20 @@ import LoginSignup from './Components/LoginSignup/LoginSignup';
 import Profile from "./Components/Profile/Profile";
 import Search from "./Components/Search/Search";
 import ActivityTracking from "./Components/ActivityTracking/ActivityTracking";
+import AddMeal from './Components/ScanFood/AddMeal';
+import ScanMeal from './Components/ScanFood/ScanMeal'
 
 function App() {
   return (
     <div>
+
       {/* Uncomment the page you want to see: */}
       <LoginSignup/>
       {/* <Profile /> */}
       {/* <Search /> */}
       {/* <ActivityTracking /> */}
+      {/* <AddMeal/> */}
+      {/* <ScanMeal/> */}
 
     </div>
   );
